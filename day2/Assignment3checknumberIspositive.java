7bc573f301b95d486503b2fe0825074adc87bea3
Mithra2024 <mithra.subramaniyan@gmail.com>
2024-11-21 21:32:58 +0530

    checking if number is positive

---
+++ day2/Assignment3checknumberIspositive.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class Assignment3checknumberIspositive {
+	public static void main(String[] args) {
+		//check if the given number is positive
+		int a=10;
+		if(a>=0)
+			System.out.println("Number is positive");
+		else
+			System.out.println("Number is negative");
+	}
+
+}
