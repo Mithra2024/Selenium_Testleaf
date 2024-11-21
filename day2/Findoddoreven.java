c9093bc82b682e993ba686ca0744a95b1f7890a1
Mithra2024 <mithra.subramaniyan@gmail.com>
2024-11-21 21:22:48 +0530

    Find odd numbers and print them-assignment2

---
+++ day2/Findoddoreven.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Findoddoreven {
+	public static void main(String[] args) {
+	int number=4;
+	if (number%2==0) {
+		System.out.println("number is even");
+	}
+	else {
+		System.out.println("Number is odd");
+	}
+
+	}
+}
+	
